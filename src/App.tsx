746e0428a68e7c511ed73156463645bacf2aadb6
viktoryaSVA <viktoriia.scherba@gmail.com>
2024-01-11 14:28:16 +0200

    Initialized react project connected with AWS Lambda

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface Result {
+  [key: string]: number;
+}
+
 function App() {
+  const [text, setText] = useState<string>('');
+  const [result, setResult] = useState<Result>({});
+
+  const handleSubmit = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+
+      if (!apiUrl) {
+        console.error('REACT_APP_API_URL is not defined');
+        return;
+      }
+
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+      });
+
+      const data = await response.json();
+
+      setResult(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <p>Input text</p>
+        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+          <p>Results</p>
+          <div className="result-container">
+          <div className="result-box">
+            {Object.entries(result).map(([key, value]) => (
+                <div key={key} className="result-item">
+                  <span>{key}:</span> <span>{value}</span>
+                </div>
+            ))}
+          </div>
+        </div>
+        <button className="submit-button" onClick={handleSubmit}>
+          Submit
+        </button>
+
+      </div>
   );
 }
 
